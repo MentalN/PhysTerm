94e10f30d8efade822260ca319ac7cd5e763c007
Nawaf Abdullah <nawaf97k@gmail.com>
2018-04-08 19:40:33 -0400

    Final

---
+++ ElectricityMagnetism/CircuitEquivalents.py
@@ -0,0 +1,149 @@
+#   File: Checkers.py
+#   Author: Nawaf Abdullah
+#   Creation Date: 7/April/2018
+#   Description: Calculating circuit equivalents for resistors, capacitors, and inductors
+from Utilities.Checkers import numeric
+
+
+#   Resistors
+def parallelResistors():
+    num_R = input("Number of parallel resistors> ")
+    try:
+        num_R = int(num_R)
+    except ValueError:
+        print("Please enter an integer value.")
+        parallelResistors()
+
+    R_eq = 0
+    for x in range(num_R):
+        print("Enter the resistance value of Resistor [", x + 1, "]")
+        R_n = input("> ")
+        try:
+            R_n = float(R_eq)
+        except ValueError:
+            R_n = numeric(R_n)
+        R_eq += 1 / R_n
+    R_eq = R_eq**-1
+
+    print("Equivalent resistance R = ", R_eq)
+
+    return R_eq
+
+
+def seriesResistors():
+    num_R = input("Number of series resistors> ")
+    try:
+        num_R = int(num_R)
+    except ValueError:
+        print("Please enter an integer value.")
+        seriesResistors()
+
+    R_eq = 0
+    for x in range(num_R):
+        print("Enter the resistance value of Resistor [", x + 1, "]")
+        R_n = input("> ")
+        try:
+            R_n = float(R_eq)
+        except ValueError:
+            R_n = numeric(R_n)
+        R_eq += R_n
+
+    print("Equivalent resistance R = ", R_eq)
+
+    return R_eq
+
+
+#   Capacitors
+def parallelCapacitors():
+    num_C = input("Number of series capacitors> ")
+    try:
+        num_C = int(num_C)
+    except ValueError:
+        print("Please enter an integer value.")
+        parallelCapacitors()
+
+    C_eq = 0
+    for x in range(num_C):
+        print("Enter the capacitance value of capacitor [", x + 1, "]")
+        C_n = input("> ")
+        try:
+            C_n = float(C_eq)
+        except ValueError:
+            C_n = numeric(C_n)
+        C_eq += C_n
+
+    print("Equivalent capacitance C = ", C_eq)
+
+    return C_eq
+
+
+def seriesCapacitors():
+    num_C = input("Number of series capacitors> ")
+    try:
+        num_C = int(num_C)
+    except ValueError:
+        print("Please enter an integer value.")
+        parallelCapacitors()
+
+    C_eq = 0
+    for x in range(num_C):
+        print("Enter the capacitance value of capacitor [", x + 1, "]")
+        C_n = input("> ")
+        try:
+            C_n = float(C_eq)
+        except ValueError:
+            C_n = numeric(C_n)
+        C_eq += 1 / C_n
+    C_eq = C_eq**-1
+
+    print("Equivalent capacitance C = ", C_eq)
+
+    return C_eq
+
+
+#   Inductors
+def parallelInductors():
+    num_L = input("Number of parallel inductors> ")
+    try:
+        num_L = int(num_L)
+    except ValueError:
+        print("Please enter an integer value.")
+        parallelInductors()
+
+    L_eq = 0
+    for x in range(num_L):
+        print("Enter the inductance value of inductors [", x + 1, "]")
+        L_n = input("> ")
+        try:
+            L_n = float(L_eq)
+        except ValueError:
+            L_n = numeric(L_n)
+        L_eq += 1 / L_n
+    L_eq = L_eq**-1
+
+    print("Equivalent inductance L = ", L_eq)
+
+    return L_eq
+
+
+def seriesInductors():
+    num_L = input("Number of series inductors> ")
+    try:
+        num_L = int(num_L)
+    except ValueError:
+        print("Please enter an integer value.")
+        seriesInductors()
+
+    L_eq = 0
+    for x in range(num_L):
+        print("Enter the capacitance value of inductors [", x + 1, "]")
+        L_n = input("> ")
+        try:
+            L_n = float(L_eq)
+        except ValueError:
+            L_n = numeric(L_n)
+        L_eq += L_n
+
+    print("Equivalent inductance C = ", L_eq)
+
+    return L_eq
