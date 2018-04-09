e9091b3423d7872f10b7e5df2b640d04f9655a74
Nawaf Abdullah <nawaf97k@gmail.com>
2018-04-09 18:49:11 -0400

    Added R_d, c, and h

--- Utilities/Constants.py
+++ Utilities/Constants.py
@@ -3,5 +3,20 @@
 #   Creation Date: 4/April/2018
 #   Description: physical constants
 
+
+#   Universal Gravitational Constant
 G   = 6.674e-11
+#   Gravitational Acceleration
 a_g = 9.80655
+#   Boltzmann constant
+k_b = 1.38066e-23
+#   Avogadro's number
+N_A = 6.0221e+23
+#   Ideal Gas Constant R (J/mol*k)
+R   = 8.3145
+#   Rydberg constant
+R_d = 1.097e+7
+#   Speed of light
+c   = 299792458
+#   Planck's constant
+h   = 6.6261e-34
