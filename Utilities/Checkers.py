a1ef6ceff41f76f90a7fd8893546c4582c32189e
Nawaf Abdullah <nawaf97k@gmail.com>
2018-04-09 18:48:40 -0400

    Added EMspectrum

--- Utilities/Checkers.py
+++ Utilities/Checkers.py
@@ -4,6 +4,7 @@
 #   Description: several functions that checks for input
 
 
+#   Checks if entry is a number and make sure it is in case if it's not
 def numeric(num):
     if isinstance(num, int) is False and isinstance(num, float) is False:
         num = input("Error, please enter a numeric value > ")
@@ -16,3 +17,20 @@ def numeric(num):
     return num
 
 
+#   Returns the wavelength position on the Electromagnetic Spectrum
+def EMspectrum(L):
+    if L <= 1e-11:
+        em_spec = "Gamma Rays"
+    elif 1e-8 >= L >= 1e-11:
+        em_spec = "X-Rays"
+    elif 390e-09 >= L >= 1e-8:
+        em_spec = "Ultraviolet"
+    elif 700e-09 <= L <= 390e-09:
+        em_spec = "Visible light"
+    elif 1e-3 <= L <= 700e-09:
+        em_spec = "Infrared"
+    elif 1e-1 <= L <= 1e-3:
+        em_spec = "Microwaves"
+    elif L >= 1e-3:
+        em_spec = "Radio waves"
+    return em_spec
