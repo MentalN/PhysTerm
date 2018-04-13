8614b0584694d75e14e9b53cb2e93b41ed516680
Nawaf Abdullah <nawaf97k@gmail.com>
2018-04-13 15:23:59 -0400

    terminal style interface

--- main.py
+++ main.py
@@ -2,8 +2,6 @@
 #   Author: Nawaf Abdullah
 #   Creation Date: 4/April/2018
 #   Description: main menu of PhysTerm. Program runtime starts here.
-from cursesmenu import *
-from cursesmenu.items import *
 # Mechanics Imports
 from Mechanics.Drag import *
 from Mechanics.Gravity import *
@@ -15,69 +13,169 @@ from Thermal.IdealGas import *
 # Quantum Mechanics Imports
 from QuantumMechanics.PhotoelectricEffect import *
 
-#   '_i'  = item in main_menu
-#   '_sm' = submenu
-
 
 def main():
-    #   Main menu declaration
-    main_menu = CursesMenu("PhysTerm", "Select a field:")
-
-    #   Mechanics: Submenu declaration
-    mech_sm = CursesMenu("Mechanics", "Select an operation:")
-    #   Mechanics: add function items for submenu
-    mech_f1 = FunctionItem("Gravity: gravitational Force", gForce)
-    mech_f2 = FunctionItem("Gravity: gravitational Potential", gPotential)
-    mech_f3 = FunctionItem("Drag: terminal velocity", terminalVelocity)
-    mech_f4 = FunctionItem("Drag: drag force", dragForce)
-    #   Mechanics: append items to Submenu
-    mech_sm.append_item(mech_f1)
-    mech_sm.append_item(mech_f2)
-    mech_sm.append_item(mech_f3)
-    mech_sm.append_item(mech_f4)
-    mech_i = SubmenuItem("Mechanics", submenu=mech_sm)
-
-    #   EM: Submenu declaration
-    em_sm = CursesMenu("ElectricityMagnetism", "Select an operation:")
-    #   EM: add function items for submenu
-    em_f1 = FunctionItem("Circuit Equivalent: parallel Resistors", parallelResistors)
-    em_f2 = FunctionItem("Circuit Equivalent: series Resistors", seriesResistors)
-    em_f3 = FunctionItem("Circuit Equivalent: parallel Capacitors", parallelCapacitors)
-    em_f4 = FunctionItem("Circuit Equivalent: series Capacitors", seriesCapacitors)
-    em_f5 = FunctionItem("Circuit Equivalent: parallel Inductors", parallelInductors)
-    em_f6 = FunctionItem("Circuit Equivalent: series Inductors", seriesInductors)
-    #   EM: append items to Submenu
-    em_sm.append_item(em_f1)
-    em_sm.append_item(em_f2)
-    em_sm.append_item(em_f3)
-    em_sm.append_item(em_f4)
-    em_sm.append_item(em_f5)
-    em_sm.append_item(em_f6)
-    em_i = SubmenuItem("ElectricityMagnetism", submenu=em_sm)
-
-    #   Thermo: Submenu declaration
-    thermo_sm = CursesMenu("Thermal Physics", "Select an operation:")
-    #   Thermo: add function items for submenu
-    thermo_f1 = FunctionItem("Ideal Gas Law", idealGas)
-    #   Thermo: append items to Submenu
-    thermo_sm.append_item(thermo_f1)
-    thermo_i = SubmenuItem("Thermal Physics", submenu=thermo_sm)
-
-    #   QM: Submenu declaration
-    qm_sm = CursesMenu("Quantum Mechanics", "Select an operation:")
-    #   QM: add function items for submenu
-    qm_f1 = FunctionItem("Photoelectric effect", transition)
-    #   QM: append items to Submenu
-    qm_sm.append_item(qm_f1)
-    qm_i = SubmenuItem("Quantum Mechanics", submenu=qm_sm)
-
-    main_menu.append_item(mech_i)
-    main_menu.append_item(em_i)
-    main_menu.append_item(thermo_i)
-    main_menu.append_item(qm_i)
-    main_menu.start()
-    main_menu.join()
-
-
-if __name__ == '__main__':
+    print("You can type 'list' for available options")
+    entry = input("PhysTerm>> ")
+    if entry.lower() == "mechanics":
+        mech()
+    elif entry.lower() == "em":
+        em()
+    elif entry.lower() == "thermal":
+        thermal()
+    elif entry.lower() == "quantum":
+        quantum()
+    elif entry.lower() == "list":
+        help_main()
+    elif entry.lower() == 'exit':
+        exit()
+    else:
+        print("Command not found!")
+        main()
     main()
+
+
+def mech():
+    def mech_help():
+        print("Entries: ")
+        print("gravity")
+        print("drag")
+        mech()
+
+    drag_dict = {"terminal velocity": terminalVelocity, "force": dragForce}
+    gravity_dict = {"force": gForce, "potential": gPotential}
+
+    entry = input("PhysTerm>Mechanics>> ")
+
+    if entry.lower() == "drag":
+        entry2 = input("PhysTerm>Mechanics>Drag>> ")
+
+        if entry2.lower() in drag_dict:
+            drag_dict[entry2]()
+        else:
+            print("Command not found!")
+            mech()
+
+    elif entry.lower() == "gravity":
+        entry2 = input("PhysTerm>Mechanics>Gravity>> ")
+
+        if entry2.lower() in gravity_dict:
+            gravity_dict[entry2]()
+        else:
+            print("Command not found!")
+            mech()
+
+    elif entry.lower() == "list":
+        mech_help()
+
+
+def em():
+    def em_help():
+        print("Entries: ")
+        print("equivalent")
+
+    eqv_dict = {"equivalent": eqv_selector}
+
+    entry = input("PhysTerm>E&M>> ")
+
+    if entry.lower() == "equivalent":
+        print("Select element: 'resistors', 'capacitors', or 'inductors'")
+        entry2 = input("PhysTerm>E&M>CircuitEquivalent>> ")
+
+        if entry2.lower() == "resistors":
+            print("select: 'parallel' or 'series'")
+            entry3 = input("PhysTerm>E&M>CircuitEquivalent>resistors>> ")
+            if entry3 == "parallel":
+                eqv_dict["equivalent"](1)
+            elif entry3 == "series":
+                eqv_dict["equivalent"](2)
+
+        elif entry2.lower() == "capacitors":
+            print("select: 'parallel' or 'series'")
+            entry3 = input("PhysTerm>E&M>CircuitEquivalent>capacitors>> ")
+            if entry3 == "parallel":
+                eqv_dict["equivalent"](3)
+            elif entry3 == "series":
+                eqv_dict["equivalent"](4)
+
+        elif entry2.lower() == "inductors":
+            print("select: 'parallel' or 'series'")
+            entry3 = input("PhysTerm>E&M>CircuitEquivalent>inductors>> ")
+            if entry3 == "parallel":
+                eqv_dict["equivalent"](5)
+            elif entry3 == "series":
+                eqv_dict["equivalent"](6)
+
+        else:
+            print("Command not found!")
+            em()
+
+    elif entry.lower() in eqv_dict:
+            eqv_dict[entry.lower()]()
+
+    elif entry.lower() == "list":
+        em_help()
+
+    elif entry.lower() == "exit":
+        exit()
+
+    else:
+        print("Command not found!")
+        em()
+
+
+def thermal():
+    def thermal_help():
+        print("Entries: ")
+        print("ideal gas")
+        thermal()
+
+    therm_dict = {"ideal gas": idealGas}
+    entry = input("PhysTerm>Thermal>> ")
+    if entry.lower() in therm_dict:
+        therm_dict[entry.lower()]()
+
+    elif entry.lower() == "list":
+        thermal_help()
+
+    elif entry.lower() == "exit":
+        exit()
+
+    else:
+        print("Command not found!")
+        thermal()
+
+
+def quantum():
+    def qm_help():
+        print("Entries: ")
+        print("transition")
+        quantum()
+
+    qm_dict = {"transition": transition}
+    entry = input("PhysTerm>Quantum>> ")
+    if entry.lower() in qm_dict:
+        qm_dict[entry.lower()]()
+
+    elif entry.lower() == "list":
+        qm_help()
+
+    elif entry.lower() == "exit":
+        exit()
+
+    else:
+        print("Command not found!")
+        quantum()
+
+
+def help_main():
+    print("Entries: ")
+    print("'mechanics' for classical mechanics")
+    print("'em' for electricity and magnetism")
+    print("'Thermal' for Thermal Physics")
+    print("'quantum' for Quantum Mechanics")
+    print("'exit' terminates the program")
+    main()
+
+
+main()
